de9e589f14b2688a02600a0d07572fab758a8907
Ivan Volkov <angrycore@gmail.com>
2025-06-11 12:55:44 +0300

    Account for the nested mounts case.

--- pathy.py
+++ pathy.py
@@ -8,6 +8,10 @@ ap.add_argument('path')
 a = ap.parse_args()
 
 u = a.path.replace('\\', '/')
+c = []
 for p in disk_partitions(all=True):
     if p.device and u.startswith(p.device):
-        print(u.replace(p.device, p.mountpoint))
+        c.append((p.device, p.mountpoint))
+if c:
+    c.sort(key=lambda s: len(s[0]), reverse=True)
+    print(u.replace(c[0][0], c[0][1]), end='')
